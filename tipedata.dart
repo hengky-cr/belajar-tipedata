19c338c1258adb670c3964e9e41f208d55651bf8
hengky-cr <zerossukanasi@gmail.com>
2026-09-08 19:22:12 +0700

    versi3

--- tipedata.dart
+++ tipedata.dart
@@ -7,9 +7,15 @@ void main(){
   // print('saya $nama, umur saya $umur tahun, berat badan saya $beratbadan kg');
 
 
-  int angka1=100;
-  double angka2=20;
-  num hasil =20;
-  print(angka2);
-  print(hasil);
+  // int angka1=100;
+  // double angka2=20;
+  // num hasil =20;
+  // print(angka2);
+  // print(hasil);
+
+  // var nilai1=100;
+  // dynamic nilai2=100;
+  // var nilai3 = true;
+  // dynamic nilai4=true;
+
 }
