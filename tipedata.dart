9036be2f272d299eecd70e00761ed75bd1472818
hengky-cr <zerossukanasi@gmail.com>
2026-09-08 19:18:40 +0700

    versi2

--- tipedata.dart
+++ tipedata.dart
@@ -1,8 +1,15 @@
 void main(){
-  String nama='Cindy';
-  int umur = 20;
-  double beratbadan = 56.5;
+  // String nama='Cindy';
+  // int umur = 20;
+  // double beratbadan = 56.5;
 
-  //saya cindy, umur saya 20 tahun, berat badan saya 56.5 kg
-  print('saya $nama, umur saya $umur tahun, berat badan saya $beratbadan kg');
+  // //saya cindy, umur saya 20 tahun, berat badan saya 56.5 kg
+  // print('saya $nama, umur saya $umur tahun, berat badan saya $beratbadan kg');
+
+
+  int angka1=100;
+  double angka2=20;
+  num hasil =20;
+  print(angka2);
+  print(hasil);
 }
